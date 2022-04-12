c5ed46e5333d19e00dff061ee3c1704caeb730f6
LucasHerculanoAmaro <lucash.96@hotmail.com>
2022-04-12 01:19:50 -0300

    Back-end Blog Pessoal

---
+++ BlogPessoal/src/main/java/com/generation/BlogPessoal/controller/PostagemController.java
@@ -0,0 +1,35 @@
+package com.generation.BlogPessoal.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.BlogPessoal.model.Postagem;
+import com.generation.BlogPessoal.repository.PostagemRepository;
+
+@RestController
+@RequestMapping ("/postagens" )
+@CrossOrigin(origins = "*", allowedHeaders = "*") // abre qualquer requisição que for solicitada
+public class PostagemController {
+	
+	@Autowired
+	private PostagemRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> GetAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@PostMapping
+	public ResponseEntity<Postagem> post (@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	}
+}
